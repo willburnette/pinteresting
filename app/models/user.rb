e17c5dbc0db8e1d9aae367bb336245e5734a234a
Will Burnette <will@hsdesignhouse.com>
2016-01-25 20:37:36 -0600

    4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-	has_attached_file :pin, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :pin, content_type: /\Aimage\/.*\Z/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
