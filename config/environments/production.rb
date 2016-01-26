a227f602ab601fb8223ef841e41f74859c9fc16d
Will Burnette <will@hsdesignhouse.com>
2016-01-25 20:59:57 -0600

    345

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: 'https://willb-pinteresting.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'willb-pinteresting.herokuapp.com' }
 
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
